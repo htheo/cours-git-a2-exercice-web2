2800a662fafa62a9c10f6a22a516078db45b7f8d
Theo Hinfray <hinfraytheo@gmail.com>
2015-10-10 11:22:40 +0200

    ajout des balises 'name' au formulaire du Login #1

--- view/login.php
+++ view/login.php
@@ -12,12 +12,12 @@
 				<form method="POST" action="login.php">
 					<div class="form-input">
 						<span class="email">
-							<input type="text" placeholder="Email">
+							<input type="text" name='email' placeholder="Email">
 						</span>
 					</div>
 					<div class="form-input">
 						<span class="password">
-							<input type="password" placeholder="Password">
+							<input type="password" name='password' placeholder="Password">
 						</span>
 					</div>
 					<div class="form-submit">
